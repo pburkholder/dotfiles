f8172e275d7fa724e23c544b785e7d5497396c9f
Peter Burkholder <peter.burkholder@gsa.gov>
2024-10-21 12:40:54 -0400

    Use popup instead of wall

--- bin/meetwarn.sh
+++ bin/meetwarn.sh
@@ -1,9 +1,11 @@
 #!/bin/sh 
 
 warn() {
-  echo Warning, $1 has been running for over an hour | wall
+  message="Warning, $1 has been running for over an hour"
+  osascript -e "tell app \"System Events\" to display dialog \"$message\""
+#  echo $message | wall
 }
 
 ps -e -o etime,command | grep "Google Meet\.app" | awk '{print $1}' | grep -q '\d:\d\d:\d\d' && warn "Meet"
+ps -e -o etime,command | grep "MacOS/zoom\.us" | awk '{print $1}' | grep -q '\d:\d\d:\d\d' && warn "Zoom"
 
-ps -e -o etime,command | grep "Macos/zoom\.us" | awk '{print $1}' | grep -q '\d:\d\d:\d\d' && warn "Zoom"
