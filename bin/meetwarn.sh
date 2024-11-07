0bba6368a7f5bbd4bc4fbbd1e08caef192a02690
Peter Burkholder <peter.burkholder@gsa.gov>
2024-11-07 09:10:53 -0500

    Don't redisplay warning

--- bin/meetwarn.sh
+++ bin/meetwarn.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
 
 warn() {
-  message="Warning, $1 has been running for over an hour"
+  ps -e -o command | grep -q "[Z]oom/Meet Warning" && return  # Already running
+  message="Zoom/Meet Warning, $1 has been running for over an hour"
   osascript -e "tell app \"System Events\" to display dialog \"$message\""
-#  echo $message | wall
 }
 
 ps -e -o etime,command | grep "Google Meet\.app" | awk '{print $1}' | grep -q '\d:\d\d:\d\d' && warn "Meet"
