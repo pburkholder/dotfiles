2de3e2cdc080bff7e2dbfe6e3bcdd51a9dc71fd9
Peter Burkholder <peter.burkholder@gsa.gov>
2024-11-12 12:24:07 -0500

    Fix meetwarn to be less chatty

--- bin/meetwarn.sh
+++ bin/meetwarn.sh
@@ -1,11 +1,13 @@
 #!/bin/sh 
 
 warn() {
-  ps -e -o command | grep -q "[Z]oom/Meet Warning" && return  # Already running
+#  ps -e -o command | grep -q "[Z]oom/Meet Warning" && return  # Already running
   message="Zoom/Meet Warning, $1 has been running for over an hour"
   osascript -e "tell app \"System Events\" to display dialog \"$message\""
 }
 
-ps -e -o etime,command | grep "Google Meet\.app" | awk '{print $1}' | grep -q '\d:\d\d:\d\d' && warn "Meet"
-ps -e -o etime,command | grep "MacOS/zoom\.us" | awk '{print $1}' | grep -q '\d:\d\d:\d\d' && warn "Zoom"
+alias g="awk '{print $1}' | grep -q '0[12]:[03]\d:\d\d'"
+
+ps -e -o etime,command | grep "Google Meet\.app" | g  && warn "Meet"
+ps -e -o etime,command | grep "MacOS/zoom\.us"   | g  && warn "Zoom"
 
